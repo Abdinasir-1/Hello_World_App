061bb0cf6aeda7b885911c30617e58411b426f63
Abdinasir-1 <71330974+Abdinasir-1@users.noreply.github.com>
2021-01-27 09:51:15 +0000

    Update python_app.py

--- python_app.py
+++ python_app.py
@@ -1,12 +1,12 @@
 import logging
 import os
+from data import env
+from data import app
 
 logging.basicConfig(filename='test.log', level=logging.INFO, 
     format='%(asctime)s:%(levelname)s:%(message)s')
 
-Nasir = os.environ.get('N_VARIABLE')
-
-msg = "Hello World"
-print(msg)
-
 logging.info(' Hello World is printed to the console. This is a log information ')
+
+print(env)
+print(app)
