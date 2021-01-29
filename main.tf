4d49d710977bcdae7bcc6eb94ed3f083359cefa6
Abdinasir-1 <71330974+Abdinasir-1@users.noreply.github.com>
2021-01-29 10:03:46 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,12 @@ resource "local_file" "hello_world" {
 import logging
 import os
 from data import env
+from data import app
 
 logging.basicConfig(filename='test.log', level=logging.INFO, 
     format='%(asctime)s:%(levelname)s:%(message)s')
 
-logging.info(' Hello World is printed to the console. This is a log information ')
+logging.info(' Hello World is successfully deployed. This is a log information ')
 
 print(env)
 print(app)
